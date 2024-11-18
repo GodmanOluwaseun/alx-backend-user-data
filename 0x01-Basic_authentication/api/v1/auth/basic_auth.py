5fe4cb110e965464c121d49d39e0fc288bfc65fc
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-11-18 16:23:58 +0100

    Add method to decode base64 strings

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,11 +3,11 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Class that inherits from Auth class"""
-
     def extract_base64_authorization_header(
         self, authorization_header: str
     ) -> str:
@@ -24,3 +24,18 @@ class BasicAuth(Auth):
         else:
             encoded_str = authorization_header.split(' ', 1)[1]
             return encoded_str
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except(TypeError, UnicodeDecodeError):
+            return None
