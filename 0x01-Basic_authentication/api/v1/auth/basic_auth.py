5edb136c2ca4dc010a90e4745cce2a869f07cfff
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-11-19 01:14:40 +0100

    Method that checks and returns the User instance.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -59,3 +60,22 @@ class BasicAuth(Auth):
 
         user_details = decoded_base64_authorization_header.split(':', 1)
         return (user_details[0],  user_details[1])
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """Returns user instance based on the credentials given"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_email, str):
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except (Exception):
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
